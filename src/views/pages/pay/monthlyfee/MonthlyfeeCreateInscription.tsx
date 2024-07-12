f8664a34af3bd3f966d2b54bddadcb9477eb7cc6
Vidal Alvarez <vidal.r.a.777@gmail.com>
2024-07-12 08:30:30 -0400

    merge and ui inscription

--- src/views/pages/pay/monthlyfee/MonthlyfeeCreateInscription.tsx
+++ src/views/pages/pay/monthlyfee/MonthlyfeeCreateInscription.tsx
@@ -12,10 +12,20 @@ import {
   DialogContent,
   DialogTitle,
   Grid,
+  TextField,
+  Box,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormControl,
+  FormLabel,
 } from '@mui/material';
 import { FormEvent, useCallback, useState } from 'react';
 import { InscriptionTable } from '../../inscription';
 
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import QrCodeIcon from '@mui/icons-material/QrCode';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 interface createProps {
   open: boolean;
   handleClose: () => void;
@@ -25,8 +35,10 @@ interface createProps {
 const formFields: FormMonthlyFeeModelInscription = {
   inscriptionId:null,
   amount: 0,
-  payMethod:'',
+  payMethod:'CASH',
   transactionNumber:'',
+  buyerName:'',
+  buyerNIT:'',
 };
 
 const formValidations: FormMonthlyFeeValidationsInscription = {
@@ -34,6 +46,10 @@ const formValidations: FormMonthlyFeeValidationsInscription = {
   amount: [(value) => value != null, 'Debe ingresar el monto'],
   payMethod: [(value) => value.length >= 1, 'Debe ingresar el methodo de pafo'],
   transactionNumber: [(value) => value.length >= 1, 'Debe ingresarel nuemro transaccion'],
+  buyerName: [(value) => value.length >= 1, 'Debe ingresar el nombre a facturar'],
+  buyerNIT: [(value) => value.length >= 1, 'Debe ingresar el NIT del cliente'],
+
+
 };
 
 export const MonthlyFeeCreateInscription = (props: createProps) => {
@@ -41,7 +57,9 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
   const {
     inscriptions,
     amount,
-    payMethod,
+    buyerName,
+    buyerNIT,
+
     transactionNumber,
     onInputChange,
     isFormValid,
@@ -49,13 +67,25 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
     onResetForm,
     inscriptionsValid,
     amountValid,
-    payMethodValid,
+    buyerNameValid,
+    buyerNITValid,
     transactionNumberValid,
   } = useForm(item ?? formFields, formValidations);
   const { createMonthlyFeeInscription, updateMonthlyFee } = useMonthlyFeeStore();
   const [formSubmitted, setFormSubmitted] = useState(false);
 
+  const [valueradio, setValueradio] = useState('');
+
+  const handleChange:any = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValueradio((event.target as HTMLInputElement).value);
+  };
+
   const sendSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    console.log("//////// SEND SUBMIT INSCRIPTIONS MONTHLY")
+    console.log({ inscriptionsId: inscriptions.id,
+      amount: parseInt(amount),
+      payMethod: valueradio,
+      transactionNumber: transactionNumber.trim(),})
     event.preventDefault();
     setFormSubmitted(true);
     if (!isFormValid) return;
@@ -63,15 +93,19 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
       await createMonthlyFeeInscription({
         inscriptionsId: inscriptions.id,
         amount: parseInt(amount),
-        payMethod: payMethod.trim(),
+        payMethod: valueradio,
         transactionNumber: transactionNumber.trim(),
+        buyerNIT: buyerNIT.trim(),
+        buyerName: buyerName.trim(),
       });
     } else {
       await updateMonthlyFee(item.id, {
         inscriptionsId: inscriptions.id,
         amount: amount.trim(),
-        payMethod: payMethod.trim(),
+        payMethod: valueradio,
         transactionNumber: transactionNumber.trim(),
+        buyerNIT: buyerNIT.trim(),
+        buyerName: buyerName.trim(),
       });
     }
     handleClose();
@@ -110,9 +144,99 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
         <DialogTitle>
           {item == null ? 'Pago Inscripcion' : `${item.id}`}
         </DialogTitle>
+        <Grid container spacing={2}>
+        <Grid item xs={12}  sx={{ padding: '5px', margin:'10px' }}>
+                <ComponentSelect
+                  label={inscriptions != null ? 'Inscripcion' : ''}
+                  title={inscriptions != null ? inscriptions.id : 'Inscripcion'}
+                  onPressed={() => handleModalInscriptions(true)}
+                  error={!!inscriptionsValid && formSubmitted}
+                  helperText={formSubmitted ? inscriptionsValid : ''}
+                />
+              </Grid>
+              </Grid>
+        <Grid container spacing={2}>
+        {inscriptions && (<>
+        <Grid item xs={4}>
+          
+            <Box sx={{padding:'10px'}}>
+              <TextField
+              
+                disabled
+                id="outlined-disabled"
+                label="id"
+                defaultValue={inscriptions.id}
+                variant="standard"
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="nombres"
+                defaultValue={`${inscriptions.student.name} ${inscriptions.student.lastName}`}
+                variant="standard"
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="inscripcion"
+                defaultValue={`${inscriptions.price.inscription}`}
+                variant="standard"
+              />
+            </Box>
+          
+        </Grid>
+        <Grid item xs={8}>
         <form onSubmit={sendSubmit}>
           <DialogContent sx={{ display: 'flex' }}>
             <Grid container>
+            <Grid item xs={12} sx={{ padding: '5px' }}>
+              <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">Forumulario</FormLabel>
+                <RadioGroup
+                 row
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  value={valueradio}
+                  onChange={handleChange}
+                  name="payMethod"
+
+                >
+                  <FormControlLabel value="CASH" control={<Radio />} label={<AttachMoneyIcon />} />
+                  <FormControlLabel value="QR" control={<Radio />} label={<QrCodeIcon />} />
+                  <FormControlLabel value="BANK" control={<Radio />} label={<AccountBalanceIcon />} />
+                </RadioGroup>
+              </FormControl>
+                {/* <ComponentInput
+                  type="text"
+                  label="payMethod"
+                  name="payMethod"
+                  value={payMethod}
+                  onChange={onInputChange}
+                  error={!!payMethodValid && formSubmitted}
+                  helperText={formSubmitted ? payMethodValid : ''}
+                /> */}
+              </Grid>
+              <Grid item xs={12} sm={6} sx={{ padding: '5px' }}>
+                <ComponentInput
+                  type="text"
+                  label="Recibe de:"
+                  name="buyerName"
+                  value={buyerName}
+                  onChange={onInputChange}
+                  error={!!buyerNameValid && formSubmitted}
+                  helperText={formSubmitted ? buyerNameValid : ''}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} sx={{ padding: '5px' }}>
+                <ComponentInput
+                  type="text"
+                  label="NIT"
+                  name="buyerNIT"
+                  value={buyerNIT}
+                  onChange={onInputChange}
+                  error={!!buyerNITValid && formSubmitted}
+                  helperText={formSubmitted ? buyerNITValid : ''}
+                />
+              </Grid>
               <Grid item xs={12} sm={6} sx={{ padding: '5px' }}>
                 <ComponentInput
                   type="number"
@@ -124,17 +248,7 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
                   helperText={formSubmitted ? amountValid : ''}
                 />
               </Grid>
-              <Grid item xs={12} sm={6} sx={{ padding: '5px' }}>
-                <ComponentInput
-                  type="text"
-                  label="payMethod"
-                  name="payMethod"
-                  value={payMethod}
-                  onChange={onInputChange}
-                  error={!!payMethodValid && formSubmitted}
-                  helperText={formSubmitted ? payMethodValid : ''}
-                />
-              </Grid>
+              
               <Grid item xs={12} sm={6} sx={{ padding: '5px' }}>
                 <ComponentInput
                   type="text"
@@ -146,15 +260,7 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
                   helperText={formSubmitted ? transactionNumberValid : ''}
                 />
               </Grid>
-              <Grid item xs={12} sm={6} sx={{ padding: '5px' }}>
-                <ComponentSelect
-                  label={inscriptions != null ? 'Inscripcion' : ''}
-                  title={inscriptions != null ? inscriptions.id : 'Inscripcion'}
-                  onPressed={() => handleModalInscriptions(true)}
-                  error={!!inscriptionsValid && formSubmitted}
-                  helperText={formSubmitted ? inscriptionsValid : ''}
-                />
-              </Grid>
+              
             </Grid>
           </DialogContent>
           <DialogActions>
@@ -169,6 +275,10 @@ export const MonthlyFeeCreateInscription = (props: createProps) => {
             <Button type="submit">{item == null ? 'CREAR' : 'EDITAR'}</Button>
           </DialogActions>
         </form>
+        </Grid>
+        </>)   }
+        </Grid>
+        
       </Dialog>
     </>
   );
